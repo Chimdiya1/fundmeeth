1999b92863d4d49b52197a57c2de6b9b1c5590a0
Chimdia Anyiam <fortunechimdiya@gmail.com>
2022-07-11 23:19:47 +0100

    initial commit

---
+++ test/staging/FundMe.staging.test.js
@@ -0,0 +1,15 @@
+const { assert, expect } = require("chai")
+const { deployments, ethers, getNamedAccounts, network } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Fundme", async function () {
+          let fundMe
+          let deployer
+          const sendValue = ethers.utils.parseEther("1")
+          beforeEach(async function () {
+              deployer = (await getNamedAccounts()).deployer
+              fundMe = await ethers.getContract("FundMe", deployer)
+          })
+      })
